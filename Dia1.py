1d73d89f7340bed384ce36ab2b1acb131759cfc2
crt-ram <ramosbarragan.cristian@gmail.com>
2023-07-27 20:26:22 -0600

    Dia1

---
+++ Dia1.py
@@ -0,0 +1,41 @@
+"""
+#Imprimir en consola
+#Primer Ejercicio
+print("Day 1 - Python Print Function")
+print("The function is declared like this")
+print("print('what to print')")
+
+#Segundo Ejercicio
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#Input
+#input("What your name?: ")
+
+#Primero hace el input y despues el print
+print("Hola " +(input("Como te llamas?: ")))
+
+#Tercer Ejercicio
+#Devuelve la longitud de los caracteres
+print(len(input("Como te llamas?: ")))
+"""
+
+#Variables
+#Declara las variables de tal manera que recuerdes que es o que cualquier persona pueda entender el codigo
+"""
+user_name=input("What is your name?: ")
+print(name)
+name_lengh=len(name)
+print(lengh)
+"""
+#Proyecto Final
+print("Hello World!, thanks for using the name band generator")
+print("What's the name of the city you grew up in?")
+city_name= input()
+print("Whats your pet name?")
+pet_name=input()
+print("Your band name could be: "+city_name+" "+pet_name)
+
+
