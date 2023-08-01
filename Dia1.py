681b0bb63621ebc0f4e50ee4ffa255811e2bcdc5
crt-ram <ramosbarragan.cristian@gmail.com>
2023-08-01 16:15:02 -0600

    formato

--- Dia1.py
+++ Dia1.py
@@ -13,11 +13,11 @@ print("New lines can be created with a backslash and n.")
 #Tercer Ejercicio
 #Devuelve la longitud de los caracteres
 print(len(input("Como te llamas?: ")))
-"""
+
 #Proyecto Final
 #Guarda en variables la ciudad del usuario y el nombre de su mascota
 #Imprime la concatenacion de la ciudad y el nombre 
-
+"""
 print("Hello World!, thanks for using the name band generator")
 print("What's the name of the city you grew up in?")
 city_name= input()
--- Dia2.py
+++ Dia2.py
@@ -3,7 +3,6 @@
 # 🚨 Don't change the code below 👇
 two_digit_number = input("Type a two digit number: ")
 # 🚨 Don't change the code above 👆
-
 ####################################
 #Write your code below this line 👇
 number1=int(two_digit_number[0])
@@ -11,10 +10,6 @@ number2=int(two_digit_number[1])
 
 result=number1+number2
 print(result)
-
-
-
-
 """
 
 
