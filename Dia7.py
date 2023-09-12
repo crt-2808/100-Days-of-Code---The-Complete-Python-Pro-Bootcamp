d315b3b0299681c35717b24335c665a31ab0e61a
crt-ram <ramosbarragan.cristian@gmail.com>
2023-09-11 22:19:09 -0600

    Dia7

--- Dia7.py
+++
@@ -1,9 +0,0 @@
-import random
-word_list=["camello", "perro","gato"] 
-selected_word=random.choice(word_list)
-guess=input("Dime una letra que pueda estar en la palabra: ").lower()
-for letter in selected_word:
-    if letter==guess:
-        print(f"There is a {guess}")
-    else:
-        print("You lost one part")
