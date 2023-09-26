37571b792c4e955f9de4ab9eacc3f50f46ba910d
Cris_Ramos <ramosbarragan.cristian@gmail.com>
2023-09-25 22:17:52 -0600

    dia9

---
+++ Dia9.py
@@ -0,0 +1,22 @@
+"""
+programming_dictionary={
+    "Bug":"Error que evita que el programa funcione correctamente",
+    "Loop": "Secuencia que se ejecuta cada cierto numero"
+}
+programming_dictionary2={
+    "Bug":"Error que evita que el programa funcione correctamente",
+    "Loop": "Secuencia que se ejecuta cada cierto numero"
+}
+# Llamada a la llave Bug
+print(programming_dictionary["Bug"])
+#Añadir un nuevo elemento
+programming_dictionary["Funcion"]="Fraccion de codigo que se puede reutilizar"
+print(programming_dictionary)
+#Limpiar un diccionario
+programming_dictionary2={}
+#Acceder a los elementos e imprimir sus llaves
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])   
+"""
+
