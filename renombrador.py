ee56205837f2b3a3eb38b08814430af5868cef25
Cris_Ramos <ramosbarragan.cristian@gmail.com>
2024-01-01 22:26:12 -0600

    Renombrador actualizado

--- renombrador.py
+++ renombrador.py
@@ -1,7 +1,23 @@
 import os
+import shutil
+
+ULTIMO_NOMBRE=0
+
+def mover_archivos(carpeta_origen, carpeta_destino):
+    if not os.path.exists(carpeta_destino):
+        os.makedirs(carpeta_destino)
+    
+    archivos = os.listdir(carpeta_origen)
+    for archivo in archivos:
+        if archivo.endswith(".jpg"):
+            ruta_origen = os.path.join(carpeta_origen, archivo)
+            ruta_destino = os.path.join(carpeta_destino, archivo)
+            shutil.move(ruta_origen, ruta_destino)
+            print(f"Moviendo {archivo} a {carpeta_destino}")
 
 carpeta = input("Ingresa la ruta de la carpeta: ")
 numero_base = int(input("Ingresa el número base: "))
+carpeta_destino = "C:/Users/ramos/OneDrive/Documentos/Mangas/vol30"  # Establece la ruta de la carpeta destino aquí
 
 if not os.path.exists(carpeta):
     print("La carpeta no existe.")
@@ -16,5 +32,13 @@ else:
             ruta_nueva = os.path.join(carpeta, nuevo_nombre)
             os.rename(ruta_antigua, ruta_nueva)
             print(f"Renombrando {archivo} a {nuevo_nombre}")
+            
+            ULTIMO_NOMBRE=nuevo_numero
     
     print("Renombrado completado.")
+    
+    # Llamada a la función para mover archivos a la carpeta destino
+    mover_archivos(carpeta, carpeta_destino)
+    print(f"Archivos movidos a {carpeta_destino}.")
+
+print (f"El numero final fue {ULTIMO_NOMBRE} y la carpeta que usaste fue {carpeta}")
