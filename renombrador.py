a89150eff5d86e60f4d6f3c5f0e25254cd3c12f8
Cris Ramos <ramosbarragan.cristian@gmail.com>
2024-03-24 19:15:22 -0600

    Cambio de num base

--- renombrador.py
+++ renombrador.py
@@ -1,7 +1,8 @@
 import os
 import shutil
 
-ULTIMO_NOMBRE=0
+ULTIMO_NOMBRE=1
+CONTINUAR=True
 
 def mover_archivos(carpeta_origen, carpeta_destino):
     if not os.path.exists(carpeta_destino):
@@ -15,30 +16,39 @@ def mover_archivos(carpeta_origen, carpeta_destino):
             shutil.move(ruta_origen, ruta_destino)
             print(f"Moviendo {archivo} a {carpeta_destino}")
 
-carpeta = input("Ingresa la ruta de la carpeta: ")
-numero_base = int(input("Ingresa el número base: "))
-carpeta_destino = "C:/Users/ramos/OneDrive/Documentos/Mangas/vol30"  # Establece la ruta de la carpeta destino aquí
 
-if not os.path.exists(carpeta):
-    print("La carpeta no existe.")
-else:
-    archivos = os.listdir(carpeta)
-    for indice, archivo in enumerate(archivos):
-        if archivo.endswith(".jpg"):
-            nuevo_numero = numero_base + indice
-            nombre_sin_extension = os.path.splitext(archivo)[0]
-            nuevo_nombre = str(nuevo_numero) + ".jpg"
-            ruta_antigua = os.path.join(carpeta, archivo)
-            ruta_nueva = os.path.join(carpeta, nuevo_nombre)
-            os.rename(ruta_antigua, ruta_nueva)
-            print(f"Renombrando {archivo} a {nuevo_nombre}")
-            
-            ULTIMO_NOMBRE=nuevo_numero
-    
-    print("Renombrado completado.")
-    
-    # Llamada a la función para mover archivos a la carpeta destino
-    mover_archivos(carpeta, carpeta_destino)
-    print(f"Archivos movidos a {carpeta_destino}.")
+while CONTINUAR:
+    print(f"Este es el nuevo numero que va a utilizar el programa {ULTIMO_NOMBRE} \n")
+    carpeta = input("Ingresa la ruta de la carpeta: ")
+    numero_base = ULTIMO_NOMBRE
+    carpeta_destino = "C:/Users/ramos/OneDrive/Documentos/kindle/prueba"  # Establece la ruta de la carpeta destino aquí
 
-print (f"El numero final fue {ULTIMO_NOMBRE} y la carpeta que usaste fue {carpeta}")
+    if not os.path.exists(carpeta):
+        print("La carpeta no existe.")
+    else:
+        archivos = os.listdir(carpeta)
+        for indice, archivo in enumerate(archivos):
+            if archivo.endswith(".jpg"):
+                nuevo_numero = numero_base + indice
+                nombre_sin_extension = os.path.splitext(archivo)[0]
+                nuevo_nombre = str(nuevo_numero) + ".jpg"
+                ruta_antigua = os.path.join(carpeta, archivo)
+                ruta_nueva = os.path.join(carpeta, nuevo_nombre)
+                os.rename(ruta_antigua, ruta_nueva)
+                print(f"Renombrando {archivo} a {nuevo_nombre}")
+
+                ULTIMO_NOMBRE=nuevo_numero
+
+        print("Renombrado completado.")
+
+        # Llamada a la función para mover archivos a la carpeta destino
+        mover_archivos(carpeta, carpeta_destino)
+        print(f"Archivos movidos a {carpeta_destino}.")
+
+    print (f"El numero final fue {ULTIMO_NOMBRE} \n y la carpeta que usaste fue {carpeta}")
+    ULTIMO_NOMBRE=ULTIMO_NOMBRE+1
+    user_selecction=input("¿Quieres seguir cambiando archivos? Escribe 'Si' para continuar: ")
+    if user_selecction.lower()!="si":
+        CONTINUAR=False
+    else:
+        CONTINUAR=True
