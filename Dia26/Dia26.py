3318bb70ae56a4035b9bf2cc89bb15d49b66167c
Cris Ramos <ramosbarragan.cristian@gmail.com>
2024-04-08 19:58:33 -0600

    Manejo de KeyError

--- Dia26/Dia26.py
+++ Dia26/Dia26.py
@@ -74,7 +74,16 @@ import pandas
 df=pandas.read_csv("./Dia26/nato_phonetic_alphabet.csv")
 
 phonetic_dict= {row.letter:row.code for (index, row) in df.iterrows()}
-word=input("Type the word you want: ").upper()
-output_list=[phonetic_dict[letter] for letter in word]
 
-print(output_list)
+def generate_phonetic():
+    word=input("Type the word you want: ").upper()
+
+    try:
+        output_list=[phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
